3ad2174920b98487cd6aa9e142b9996f6419ac96
Godwin <godwinaugustine118@gmail.com>
2026-07-30 23:20:56 +0100

    added the render url to the frontend .env file

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,21 @@ import app from "./app.js";
 import connectDB from "./config/db.js";
 import seedAdmin from "./seedAdmin.js";
 
-await connectDB();
-
-await seedAdmin();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await connectDB();
+
+    await seedAdmin();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+startServer();
